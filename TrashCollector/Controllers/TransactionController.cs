2a45d994d069d70c599d28b2ec4046388d7711b1
Jacob Hansen <hansenjacobs@gmail.com>
2018-06-10 12:48:34 -0500

    completed Home.Index for anon & customer & Transaction.Index

---
+++ TrashCollector/Controllers/TransactionController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TrashCollector.Models;
+using Microsoft.AspNet.Identity;
+using System.Data.Entity;
+
+namespace TrashCollector.Controllers
+{
+    [Authorize]
+    public class TransactionController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public TransactionController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [Authorize(Roles = RoleName.Customer)]
+        // GET: Transaction
+        public ActionResult Index()
+        {
+            var userId = User.Identity.GetUserId();
+            var viewModel = new CustomerTransactionViewModel()
+            {
+                Customer = _context.Customers.Single(c => c.UserId == userId),
+                Transactions = _context.Transactions
+                    .Where(t => t.UserAccountId == userId)
+                    .OrderByDescending(t => t.TransactionDateTime).ToList(),
+                Balance = Customer.GetCurrentBalance(_context, userId)
+            };
+            return View(viewModel);
+        }
+    }
+}
---
+++ TrashCollector/Models/CustomerTransactionViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TrashCollector.Models
+{
+    public class CustomerTransactionViewModel
+    {
+        public Customer Customer { get; set; }
+        public List<Transaction> Transactions { get; set; }
+        public double Balance { get; set; }
+    }
+}
